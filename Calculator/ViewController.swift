3ec7dfaac0fec5f989b575bd6538183d92b49758
Interns <interns@Revelations-MacBook-Pro.local>
2016-01-09 15:53:08 -0800

    Add division, addition, and subtraction

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,57 +13,125 @@ class ViewController: UIViewController {
     @IBOutlet weak var display: UILabel!
     
     var userIsInTheMiddleOfTypingANumber = false
+    var operandStack = [String]()
+    var sumInDisplay = false
+    var operatorJustUsed = false
+
 
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
+        
         if userIsInTheMiddleOfTypingANumber {
             display.text = display.text! + digit
+        
+            if operandStack.count > 0 && operatorJustUsed == false {
+                operandStack.append("\(operandStack.removeLast())" + "\(digit)")
+            }
+            else {
+                operandStack.append(digit)
+                operatorJustUsed = false
+            }
         }
         else {
             display.text = digit
             userIsInTheMiddleOfTypingANumber = true
+            operandStack.append(digit)
+            
         }
     }
-    var operandStack = Array<Double>()
+   
+    
+    @IBAction func appendOperator(sender: UIButton) {
+        let sign = sender.currentTitle!
+        operatorJustUsed = true
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + sign
+            operandStack.append(sign)
+        }
+        else if sumInDisplay {
+            display.text = display.text! + sign
+            operandStack.append(sign)
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
     
     @IBAction func enter() {
+        sumInDisplay = true
+        println("stack = \(operandStack)")
+        var total: Double = 0
+        var i = 0
+        while contains(operandStack, "✖️") {
+            switch operandStack[i] {
+            case "✖️":
+                total = NSNumberFormatter().numberFromString(operandStack[i-1])!.doubleValue * NSNumberFormatter().numberFromString(operandStack[i+1])!.doubleValue
+                operandStack.removeRange(i-1...i)
+                if operandStack.count > 2 {
+                    operandStack[i-1] = "\(total)"
+                }
+                
+                i = 0
+                println("stack = \(operandStack)")
+            default: break
+            }
+            i++
+        }
+        while contains(operandStack, "➗") {
+            switch operandStack[i] {
+            case "➗":
+                total = NSNumberFormatter().numberFromString(operandStack[i-1])!.doubleValue / NSNumberFormatter().numberFromString(operandStack[i+1])!.doubleValue
+                operandStack.removeRange(i-1...i)
+                if operandStack.count > 2 {
+                    operandStack[i-1] = "\(total)"
+                }
+                
+                i = 0
+                println("stack = \(operandStack)")
+            default: break
+            }
+            i++
+        }
+        while contains(operandStack, "➕") {
+            switch operandStack[i] {
+            case "➕":
+                total = NSNumberFormatter().numberFromString(operandStack[i-1])!.doubleValue + NSNumberFormatter().numberFromString(operandStack[i+1])!.doubleValue
+                operandStack.removeRange(i-1...i)
+                if operandStack.count > 2 {
+                    operandStack[i-1] = "\(total)"
+                }
+
+                i = 0
+                println("stack = \(operandStack)")
+            default: break
+            }
+            i++
+        }
+        while contains(operandStack, "➖") {
+            switch operandStack[i] {
+            case "➖":
+                total = NSNumberFormatter().numberFromString(operandStack[i-1])!.doubleValue - NSNumberFormatter().numberFromString(operandStack[i+1])!.doubleValue
+                operandStack.removeRange(i-1...i)
+                if operandStack.count > 2 {
+                    operandStack[i-1] = "\(total)"
+                }
+                
+                i = 0
+                println("stack = \(operandStack)")
+            default: break
+            }
+            i++
+        }
+
+        display.text = "\(total)"
+        operandStack = []
+        operandStack.append("\(total)")
         userIsInTheMiddleOfTypingANumber = false
-        operandStack.append(displayValue)
-        println("\(displayValue)")
-        
+        println("stack = \(operandStack)")
     }
     
-    @IBAction func operate(sender: UIButton) {
-       let operation = sender.currentTitle!
-        if userIsInTheMiddleOfTypingANumber {
-            enter()
-        }
-        
-        switch operation {
-        case "✖️": performOperation {$0 * $1}
-        case "➗": performOperation {$1 / $0}
-        case "➕": performOperation {$0 + $1}
-        case "➖": performOperation {$1 - $0}
-        default: break
-        }
-    }
-    
-    func performOperation(operation: (Double, Double) -> Double) {
-        if operandStack.count >= 2 {
-          displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
-          enter()
-        }
-        
-    }
-    var displayValue: Double {
-        get {
-           return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
-        }
-        
-        set {
-            display.text = "\(newValue)"
-            userIsInTheMiddleOfTypingANumber = false
-        }
+    @IBAction func clear() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack = []
+        display.text = "0"
     }
 }
 
